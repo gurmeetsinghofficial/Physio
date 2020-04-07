babf51a7c8f0711dc8d5c6a42a8127649c23f91a
gangasingh0001 <gangasingh.cse16@chitkarauniversity.edu.in>
2020-04-07 20:14:24 +0530

    setup for node JS Backend

---
+++ db/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+// mongoose.connect("mongodb+srv://ashishrawat:ashish@node-exam-portal-9ma4e.mongodb.net/test?retryWrites=true&w=majority", {
+// useUnifiedTopology: true,
+// useNewUrlParser: true,
+// })
+// .then(() => console.log('DB Connected!'))
+// .catch(err => {
+// console.log(err.message);
+// });
